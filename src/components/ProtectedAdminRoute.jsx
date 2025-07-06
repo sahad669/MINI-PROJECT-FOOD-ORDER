bdab561006ecc306f84139f1e0025057f5688180
sahad <sahadmosco@gmail.com>
2025-07-07 00:20:45 +0400

    added

--- src/components/ProtectedAdminRoute.jsx
+++ src/components/ProtectedAdminRoute.jsx
@@ -17,3 +17,20 @@ const ProtectedAdminRoute = ({ children }) => {
 
 export default ProtectedAdminRoute;
 
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import productReducer from "../features/productSlice"
+// import  cartReducer from "../features/cartSlice"
+// import  authReducer from "../features/authSlice"
+// import themeReducer from "../features/themeSlice"
+
+ 
+
+// export const Store = configureStore({
+//     reducer: {
+//         products: productReducer,
+//         cart: cartReducer,
+//         auth: authReducer,
+//         theme:themeReducer,
+//     }
+// })
