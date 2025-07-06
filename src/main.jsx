9a7074d717aef489f5d60cfe890000f78a57f751
sahad <sahadmosco@gmail.com>
2025-07-06 10:30:20 +0400

    double chek store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { Store } from "./app/Store.js";  // ✅ Match this with your actual file name
+import { Store } from "./app/Store.js"; 
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
